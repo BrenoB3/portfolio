97299643f1d9ba00b9e678243561df0cd5925b79
SERVIDOR\breno <brenobbb26@gmail.com>
2025-01-08 21:13:00 -0300

    # Changes to be committed: #	modified:   src/app/app.component.ts #	modified:   src/app/components/home/home.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, Inject, OnInit, PLATFORM_ID, AfterViewInit, Renderer2 } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,53 +8,6 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent {
   title = 'Portfolio';
-
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object,
-    private renderer: Renderer2
-  ) { }
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.observeImages();
-    }
-  }
-
-  ngAfterViewInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      setTimeout(() => {
-        this.addClassToContactItems();
-      }, 1000);
-    }
-  }
-
-  observeImages() {
-    if (isPlatformBrowser(this.platformId)) {
-      const images = document.querySelectorAll('.image-container');
-
-      const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            this.renderer.addClass(entry.target, 'slide-in');
-            observer.unobserve(entry.target);
-          }
-        });
-      }, {
-        threshold: 0.5
-      });
-
-      images.forEach(image => {
-        observer.observe(image);
-      });
-    }
-  }
-
-  private addClassToContactItems() {
-    const contactItems = document.querySelectorAll('.contact-item');
-    contactItems.forEach(item => {
-      this.renderer.addClass(item, 'show');
-    });
-  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID, AfterViewInit, Renderer2 } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
@@ -7,6 +8,51 @@ import { NavbarComponent } from '../navbar/navbar.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private renderer: Renderer2
+  ) { }
 
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.observeImages();
+    }
+  }
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        this.addClassToContactItems();
+      }, 1000);
+    }
+  }
+
+  observeImages() {
+    if (isPlatformBrowser(this.platformId)) {
+      const images = document.querySelectorAll('.image-container');
+
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.renderer.addClass(entry.target, 'slide-in');
+            observer.unobserve(entry.target);
+          }
+        });
+      }, {
+        threshold: 0.5
+      });
+
+      images.forEach(image => {
+        observer.observe(image);
+      });
+    }
+  }
+
+  private addClassToContactItems() {
+    const contactItems = document.querySelectorAll('.contact-item');
+    contactItems.forEach(item => {
+      this.renderer.addClass(item, 'show');
+    });
+  }
 }
