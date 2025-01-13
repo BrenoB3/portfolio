bc3635313b3f4826cdcdb0050ec26665460ce647
SERVIDOR\breno <brenobbb26@gmail.com>
2025-01-13 13:04:18 -0300

    # Changes to be committed: #	modified:   src/app/app.routes.ts #	modified:   src/app/components/navbar/navbar.component.html #	new file:   src/app/components/skill/skill.component.html #	new file:   src/app/components/skill/skill.component.scss #	new file:   src/app/components/skill/skill.component.spec.ts #	new file:   src/app/components/skill/skill.component.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { SkillComponent } from './components/skill/skill.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'skill', component: SkillComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
