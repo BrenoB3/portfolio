11d07d688d6ca72ab9410f1cdc5c8823cfbb2ffe
SERVIDOR\breno <brenobbb26@gmail.com>
2025-01-13 14:51:12 -0300

    # Changes to be committed: #	modified:   src/app/components/skill/skill.component.html #	modified:   src/app/components/skill/skill.component.scss #	modified:   src/app/components/skill/skill.component.ts #	new file:   src/assets/java.png #	new file:   src/assets/typescript.png

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skill',
-  imports: [],
+  standalone: true, // Tornando o componente independente
+  imports: [CommonModule], // Adicionando CommonModule
   templateUrl: './skill.component.html',
-  styleUrl: './skill.component.scss'
+  styleUrls: ['./skill.component.scss']
 })
 export class SkillComponent {
-
+  // Lista de imagens
+  images: string[] = [
+    'assets/java.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+    'assets/typescript.png',
+  ];
 }
