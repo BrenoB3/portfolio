09da1e4f0dc054958c192dd8855b5b7d77908716
SERVIDOR\breno <brenobbb26@gmail.com>
2025-01-14 13:28:42 -0300

    remove BackgroundComponent and update contact section layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillComponent } from './components/skill/skill.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { BackgroundComponent } from './components/background/background.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'skill', component: SkillComponent },
     { path: 'contact', component: ContactComponent },
-    { path: 'back', component: BackgroundComponent },
     { path: '**', component: HomeComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
