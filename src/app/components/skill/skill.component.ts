7ae4ce658bc0537afd1b87f01d0652ea066ae52f
SERVIDOR\breno <brenobbb26@gmail.com>
2025-01-13 21:27:02 -0300

    # Changes to be committed: #	modified:   src/app/components/about/about.component.html #	modified:   src/app/components/skill/skill.component.html #	modified:   src/app/components/skill/skill.component.scss #	modified:   src/app/components/skill/skill.component.ts #	new file:   src/assets/flutter.png #	new file:   src/assets/git.png

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -3,44 +3,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skill',
-  standalone: true, // Tornando o componente independente
-  imports: [CommonModule], // Adicionando CommonModule
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './skill.component.html',
   styleUrls: ['./skill.component.scss']
 })
 export class SkillComponent {
-  // Lista de imagens
   images: string[] = [
     'assets/java.png',
     'assets/typescript.png',
     'assets/js.png',
     'assets/dart.png',
     'assets/delphi.png',
-
+    'assets/flutter.png',
     'assets/angular.png',
     'assets/springboot.png',
-
     'assets/css.png',
     'assets/html.png',
-
     'assets/mysql.png',
     'assets/postgresql.png',
-
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
-    'assets/typescript.png',
+    'assets/git.png',
   ];
 }
